ea38113c7e6870a9c44ab1515d084f495e572ff6
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2022-09-14 23:52:13 +0700

    web localhost sudah berhasil terbentuk

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'id': '2106702756',
+        'nama': 'Fauzan Firzandy Khifzan'
+    }
+
+    return render(request, "katalog.html", context)
