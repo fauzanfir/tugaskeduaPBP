f97d4e4aa13aa8862f07c8e551c0bad2e0eb8ab5
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2022-09-21 12:34:41 +0700

    Aplikasi kedua

---
+++ mywatchlist/models.py
@@ -0,0 +1,10 @@
+from xmlrpc.client import Boolean
+from django.db import models
+
+# Create your models here.
+class MyWatchList(models.Model):
+    watched = models.BooleanField()
+    title = models.TextField()
+    rating = models.IntegerField()
+    release_date = models.TextField()
+    review = models.TextField()
