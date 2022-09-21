d030e168abb2e30f7e8e1a45f747a0db3f6d25de
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2022-09-21 23:21:26 +0700

    Menambahkan unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from audioop import reverse
+from django.test import TestCase, Client
+from django.urls import reverse
+class WatchListTestCase(TestCase):
+    def test(self):
+        html_response = Client().get(reverse('mywatchlist:show_mywatchlist'))
+        xml_response = Client().get(reverse('mywatchlist:show_xml'))
+        json_response = Client().get(reverse('mywatchlist:show_json'))
+        self.assertEqual(html_response.status_code, 200)
+        self.assertEqual(xml_response.status_code, 200)
+        self.assertEqual(json_response.status_code, 200)
 
-# Create your tests here.
+
+    
