a6847c52eb8c32e655b9984aa8772d7b1a27585d
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2022-10-13 10:17:54 +0700

    Mengimplementasi ajax

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from todolist.views import create_task, delete_task, register, show_task, update_task
 from todolist.views import login_user
 from todolist.views import logout_user
+from todolist.views import post_ajax_todolist, show_json, show_json_by_id, show_todolist_ajax
 
 app_name = 'todolist'
 
@@ -10,7 +11,11 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('create-task/', create_task, name='create_task'),
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('add/', create_task, name='create_task'),
     path('update-task/<int:id>', update_task, name='update_task'),
     path('delete-task/<int:id>', delete_task, name='delete_task'),
+    path('ajax/', show_todolist_ajax, name='show_todolist_ajax'),
+    path('ajax/submit', post_ajax_todolist, name="post_ajax_todolist")
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -10,6 +10,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from todolist.models import Task
 from todolist.forms import TaskForm
+import json
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 
 
 
@@ -79,9 +82,43 @@ def update_task(request, id):
     task = task_list[0]
     task.is_finished = not task.is_finished
     task.save()
-    return redirect('todolist:show_task')
+    return redirect('todolist:show_todolist_ajax')
 
 @login_required(login_url='/todolist/login/')
 def delete_task(request, id):
     Task.objects.filter(id=id).delete()
-    return redirect('todolist:show_task')
+    return redirect('todolist:show_todolist_ajax')
+
+@login_required(login_url='/todolist/login/')
+def show_todolist_ajax(request):
+    task_list = Task.objects.all()
+    context = {
+    'list_task': task_list,
+    'last_login': request.COOKIES['last_login']
+    }
+    return render(request, "todolist_ajax.html", context)
+
+
+def post_ajax_todolist(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+
+        ins = Task(title=title, description=description)
+        ins.save()
+
+        data = {
+            "message" : 'Submitted!'
+        }
+        json_obj = json.dumps(data, indent = 4)
+
+        return JsonResponse(json.loads(json_obj))
+    return render(request, "create_task.html")
+
+def show_json(request):
+    data = Task.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = Task.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
